4b7fb64f32af51b531096381225a04f94a2af48c
Jeremy-YI <yiyanhong10233@gmail.com>
2023-05-22 11:35:15 +1000

    feat: modify

--- src/pages/HeaderLayout/index.tsx
+++ src/pages/HeaderLayout/index.tsx
@@ -1,16 +1,35 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import List from '@mui/material/List'
+import { Link, Outlet } from 'react-router-dom'
+import { Box, List, ListItemText } from '@mui/material'
 const HeaderLayout = () => {
   return (
     <>
-      <List>
-        <Link to="/">Home</Link>
-        <Link to="/categories">Categories</Link>
-        <Link to="/location">Location</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/login">Login</Link>
-      </List>
+      <Box>
+        <List
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Link to="/">
+            <ListItemText primary="Home" />
+          </Link>
+          <Link to="/categories">
+            <ListItemText primary="Categories" />
+          </Link>
+          <Link to="/location">
+            <ListItemText primary=" Location" />
+          </Link>
+          <Link to="/register">
+            <ListItemText primary=" Register" />
+          </Link>
+          <Link to="/login">
+            <ListItemText primary=" Login" />
+          </Link>
+        </List>
+      </Box>
+      <Outlet />
     </>
   )
 }
