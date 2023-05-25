511148635b3c3a3f7d98182ed7599c0a14a90087
maggieAU1314 <lavendermaggie1314@outlook.com>
2023-05-25 22:46:54 +1000

    “AB#34homepage-banner”

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,6 +1,69 @@
-import React from 'react'
-const HomePage = () => {
-  return <div>This is home page</div>
+import React, { ChangeEvent } from 'react'
+import Button from '@mui/joy/Button'
+import SelectBox from '../../components/SelectBox'
+
+// interface Option {
+//   value: number
+//   label: string
+// }
+
+// interface SelectBoxProps {
+//   label: string
+//   options: Option[]
+//   value: number
+//   onChange: (event: ChangeEvent<{ value: unknown }>) => void
+// }
+
+// const SelectBox: React.FC<SelectBoxProps> = ({
+//   label,
+//   options,
+//   value,
+//   onChange,
+// }) => {
+//   return (
+//     <form>
+//       <label>{label}</label>
+//       <select value={value} onChange={onChange}>
+//         {options.map((option) => (
+//           <option key={option.value} value={option.value}>
+//             {option.label}
+//           </option>
+//         ))}
+//       </select>
+//     </form>
+//   )
+// }
+
+const HomePage: React.FC = () => {
+  const handleChange = (event: ChangeEvent<{ value: unknown }>) => {
+    // Handle the change event here
+  }
+
+  return (
+    <>
+      <SelectBox
+        label="product"
+        options={[
+          { value: 1, label: 'optionOne' },
+          { value: 2, label: 'optionTwo' },
+          { value: 3, label: 'optionThree' },
+        ]}
+        value={1}
+        onChange={handleChange}
+      />
+      <SelectBox
+        label="product"
+        options={[
+          { value: 4, label: 'optionFour' },
+          { value: 5, label: 'optionFive' },
+          { value: 6, label: 'optionSix' },
+        ]}
+        value={4}
+        onChange={handleChange}
+      />
+      <Button>SEARCH</Button>
+    </>
+  )
 }
 
 export default HomePage
